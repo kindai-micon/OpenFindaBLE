222e917c7f0de5039a5b6e5bc3b706cb4e60f09e
katiware <zhongjiashengwu@gmail.com>
2025-07-09 23:47:32 +0900

    [add]デバイスとユーザーデバイスのモデルの追加

---
+++ OpenFindaBLE/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenFindaBLE.Backend.Models
+{
+    public class Device:BaseEntity
+    {
+        public byte[]? EncryptionKey { get; set; }
+        public byte[]? VarificationKey { get; set; }
+        public Device()
+        {
+            EncryptionKey = new byte[32]; // Default size for AES-256
+            VarificationKey = new byte[32]; // Default size for AES-256
+        }
+
+    }
+}
---
+++ OpenFindaBLE/Models/UserDevice.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenFindaBLE.Backend.Models
+{
+    public class UserDevice : BaseEntity
+    {
+        [ForeignKey(nameof(UserId))]
+
+        public ApplicationUser UserId { get; set; }
+        [ForeignKey(nameof(DeviceId))]
+        public Device DeviceId { get; set; }
+
+        public Attributes Attribute { get; set; } = Attributes.None;
+        public enum Attributes
+        {
+            None = 0,
+            Owner = 1,
+            Outsiders = 2,
+        }
+    }
+}
