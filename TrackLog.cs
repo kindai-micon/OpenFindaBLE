c57f3efd279797957e0a5f3dc972bbecce269845
MIRA_PC\jianr <jianrenbeili52@gmail.com>
2025-07-13 23:58:16 +0900

    TrackLogの変数を追加　BaseEntityの=の誤字を修正　

---
+++ TrackLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenFindaBLE.Backend.Models
+{
+	public class TrackLog
+	{
+		[Key]
+		public Guid Id { get; set; };
+		public double Latitude { get; set; };
+		public double Longitude { get; set; };
+		public double AccelerationX { get; set; };
+		public double AccelerationY { get; set; };
+		public double AccelerationZ { get; set; };
+		public double GyroX { get; set; };
+		public double GyroY { get; set; };
+		public double GyroZ { get; set; };
+		public DateTime ReceiveTime { get; set; };
+		public int Reliability { get; set; };
+		public Guid DeviceId { get; set; };
+		public TrackLog()
+		{
+			
+		}
+	}
+}
