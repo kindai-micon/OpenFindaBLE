6ff3fcafb840d294f09138de38f91b36dbdee7c7
MIRA_PC\jianr <jianrenbeili52@gmail.com>
2025-09-10 20:45:44 +0900

    特定のユーザの緯度経度を返すメソッドを追加
    権限は未実装

---
+++ OpenFindaBLE/Controllers/TrackLogController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace OpenFindaBLE.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrackLogController(ApplicationDbContext dbContext) : ControllerBase
+    {
+        
+        [Authorize(Policy = "TrackLogController")]
+
+        [HttpPost(nameof(GetLocationLog))]
+        public async Task<List<string>> GetLocationLog(Guid id)
+        {
+            var logs = await dbContext.TrackLogs.Where(x => x.Id == id).ToListAsync();
+            var locations = new List<string>();
+            logs.ForEach(x =>
+            {
+                var location = $"{x.Longitude},{x.Latitude}";
+                locations.Add(location);
+            });
+            if (logs == null)
+            {
+                return null;
+            }
+            
+            return locations;
+        }
+    }
+}
